3edc047d50ded682cf1c7e4d7ae72e1d7f300d34
runtome <suphot.n@gmail.com>
2023-06-02 14:41:13 +0700

    Movie List & Movie Card Components,

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import { getSession, signOut } from "next-auth/react";
 
 import useCurrentUser from "@/hooks/useCurrentUser";
 import Navbar from "@/components/Navbar";
-import Billboard from '@/components/Billboard'
+import Billboard from '@/components/Billboard';
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -24,12 +26,15 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
   const { data : user } = useCurrentUser();
-  console.log(user)
+  const {data : movies =[] } = useMovieList();
 
   return (
     <>
       <Navbar />
       <Billboard  />
+      <div className="pb-40">
+        <MovieList title = "trending NoW" data = {movies} />
+      </div>
     </>
   )
 }
